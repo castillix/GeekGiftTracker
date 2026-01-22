54b9047b2be60304e14d4d0af45497953a0ecfbf
Barrett Daniel <barrett@freegeekarkansas.org>
2026-01-22 16:39:40 -0600

    fix cors hopefully

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex='https?://.*',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
