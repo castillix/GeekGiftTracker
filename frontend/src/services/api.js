f5066f88d9393db9a19a2c33b766287493346f58
Barrett Daniel <barrett@freegeekarkansas.org>
2026-01-29 10:39:30 -0600

    fix db call issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:15003';
+const API_PORT = '15003';
+const API_URL = `http://${window.location.hostname}:${API_PORT}`;
 
 export const api = axios.create({
     baseURL: API_URL,
